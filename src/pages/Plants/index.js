27bf9ef80d26e23f01c8c07c6683df52268d0b86
kev1nnnnnnn <kevinbfv@gmail.com>
2021-11-01 18:49:12 -0400

    stackNavigator

---
+++ src/pages/Plants/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
+import styles from './style'
+
+export default function Plants({navigation}){
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.contentTop}>
+            <Text style={styles.titleAlbum}>Tipos de frutas</Text>
+            <Text style={styles.textAlbum}>As frutas podem ser classificadas em três tipos principais: carnosos secos e citrícas. </Text>
+            </View>
+
+
+        <ScrollView style={styles.contentScroll}>
+         
+
+            <Text style={styles.titleScroll}>Frutos carnosos</Text>
+            <Text style={styles.textScroll}>Os frutos carnosos, são aqueles que, quando maduros apresentam consistência macia.</Text>
+            <Image 
+            style={styles.albumImage}
+            source={require('../../../assets/frutas-carnosas.jpg')}
+            />
+            
+            <View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Information')}
+            >
+                <Text style={styles.textButton}>Mais informações</Text>
+            </TouchableOpacity>
+            </View>
+
+            <Text style={styles.titleScroll}>Frutas secas</Text>
+            <Text style={styles.textScroll}>A principal característica das frutas secas é a de não apresentar polpa macia.</Text>
+            <Image 
+            style={styles.albumImage}
+            source={require('../../../assets/frutas-secas.jpg')}
+            />
+             <View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={[]}
+            >
+                <Text style={styles.textButton}>Mais informações</Text>
+            </TouchableOpacity>
+            </View>
+            
+            <Text style={styles.titleScroll}>Frutas cítricas ou frutas ácidas</Text>
+            <Text style={styles.textScroll}>As frutas cítricas, também chamadas de frutas ácidas ou citrinos, têm como principal característica a acidez que resulta em sabor ligeiramente azedo.</Text>
+            <Image 
+            style={styles.albumImage}
+            source={require('../../../assets/frutas-citricas.jpg')}
+            />
+             <View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={[]}
+            >
+                <Text style={styles.textButton}>Mais informações</Text>
+            </TouchableOpacity>
+            </View>
+            
+            
+        </ScrollView>
+    </View>
+    );
+    
+}
---
+++ src/pages/Plants/style.js
@@ -0,0 +1,72 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+    container: {
+
+        backgroundColor: "#004953",
+    },
+    albumImage: {
+        width: 250,
+        height: 200,
+        marginLeft: "20%",
+        marginRight: "20%",
+        borderRadius: 20,
+    },
+    contentTop: {
+        marginTop: "0%",
+        backgroundColor: "#20B2AA",
+        borderBottomEndRadius: 30,
+        borderBottomStartRadius: 30,
+        paddingTop: "5%",
+    },
+    titleAlbum: {
+        fontSize: 26,
+        fontWeight: "bold",
+        color: "#ffff",
+        textAlign: "center"
+    },
+    textAlbum: {
+        fontSize: 18,
+        color: "#fff",
+        lineHeight: 25,
+        paddingTop: "2%",
+        marginBottom: "10%",
+        textAlign: "center"
+    },
+
+    contentScroll: {
+
+        marginTop: "5%",
+    },  
+    titleScroll: {
+        fontSize: 26,
+        color: "#fff",
+        marginLeft: "3%",
+        marginRight: "3%",
+    },
+    textScroll: {
+        fontSize: 16,
+        color: "#fff",
+        lineHeight: 25,
+        marginBottom: "5%",
+        marginLeft: "3%",
+        marginRight: "3%",
+    },
+    button: {
+        padding: 15,
+        backgroundColor: "#20B2AA",
+        marginLeft: "15%",
+        marginRight: "15%",
+        borderRadius: 25,
+        marginTop: "5%",
+        alignItems: "center"
+    },
+    textButton: {
+        color: "#fff",
+        fontWeight: "bold",
+    }
+
+});
+
+export default styles
